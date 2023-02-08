af9bc54ae8223e01a3df9a69977e2a40e87cd147
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-02-08 16:02:17 +0530

    Subscription digest component added

---
+++ src/components/Home/Digest/Digest.tsx
@@ -0,0 +1,16 @@
+import './Digest.scss';
+function Digest() {
+  return (
+    <div className="digest">
+      <div className="digest-card">
+        <img src="/img/digest.png" />
+        <div className="subscribe">
+          <h2>Join Our Weekly Digest</h2>
+          <h3>Get exclusive promotions & updates straight to your inbox.</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Digest;
