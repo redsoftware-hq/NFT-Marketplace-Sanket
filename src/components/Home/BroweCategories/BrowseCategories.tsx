ebd6f2cecb9d5c1b1d5878e1d3eae3bf485f9c5f
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-02-08 16:01:38 +0530

    Browsing category component added

---
+++ src/components/Home/BroweCategories/BrowseCategories.tsx
@@ -0,0 +1,60 @@
+import './BrowseCategories.scss';
+const categories = [
+  {
+    title: 'Art',
+    icon: '/img/categories/c1.png',
+    altIcon: 'Art Icon',
+  },
+  {
+    title: 'Collectibles',
+    icon: '/img/categories/c2.png',
+    altIcon: 'Collectibles Icon',
+  },
+  {
+    title: 'Music',
+    icon: '/img/categories/c3.png',
+    altIcon: 'Music Icon',
+  },
+  {
+    title: 'Photography',
+    icon: '/img/categories/c4.png',
+    altIcon: 'Photography Icon',
+  },
+  {
+    title: 'Art',
+    icon: '/img/categories/c1.png',
+    altIcon: 'Art Icon',
+  },
+  {
+    title: 'Collectibles',
+    icon: '/img/categories/c2.png',
+    altIcon: 'Collectibles Icon',
+  },
+  {
+    title: 'Music',
+    icon: '/img/categories/c3.png',
+    altIcon: 'Music Icon',
+  },
+  {
+    title: 'Photography',
+    icon: '/img/categories/c4.png',
+    altIcon: 'Photography Icon',
+  },
+];
+function BrowseCategories() {
+  return (
+    <div className="categories">
+      <h2>Browse Categories</h2>
+      <div className="category-container">
+        {categories.map((category, index) => (
+          <div key={index} className="category-card">
+            <img src={category.icon} alt={category.altIcon} />
+            <h3>{category.title}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default BrowseCategories;
