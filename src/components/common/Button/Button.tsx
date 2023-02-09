e580c22be22012ca3cfc00a72d5f838f7b3e22e4
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-02-09 17:48:45 +0530

    img would still render when none is provided

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,20 +1,19 @@
-import {  MouseEventHandler } from 'react'
-import './Button.scss'
-
+import { MouseEventHandler } from "react";
+import "./Button.scss";
 
 interface Button {
-    btnName: string,
-    btnType: 'secondary-filled' | 'secondary-outlined',
-    icon?:  any,
-    handleClick?: MouseEventHandler,
+  btnName: string;
+  btnType: "secondary-filled" | "secondary-outlined";
+  icon?: any;
+  handleClick?: MouseEventHandler;
 }
-function Button({btnName,btnType,handleClick,icon}:Button) {
+function Button({ btnName, btnType, handleClick, icon }: Button) {
   return (
-    <button  className={btnType} onClick={handleClick}> 
-    <img src={icon}/>
-    {btnName}
+    <button className={btnType} onClick={handleClick}>
+      {!icon ? <></> : <img src={icon} />}
+      {btnName}
     </button>
-  )
+  );
 }
 
-export default Button
+export default Button;
