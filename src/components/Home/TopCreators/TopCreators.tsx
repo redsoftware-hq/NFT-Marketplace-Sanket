667d2ee2c2fac35f51dd351d9d9a22d69aafeeb1
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-02-09 17:45:24 +0530

    Missing ui element creator rank

--- src/components/Home/TopCreators/TopCreators.tsx
+++ src/components/Home/TopCreators/TopCreators.tsx
@@ -1,54 +1,54 @@
-import Button from '../../common/Button/Button';
-import RocketLaunch from '../../../assests/icons/RocketLaunch.svg';
-import './TopCreators.scss';
+import Button from "../../common/Button/Button";
+import RocketLaunch from "../../../assests/icons/RocketLaunch.svg";
+import "./TopCreators.scss";
 const creators = [
   {
-    name: 'Keepitreal',
-    rank: '1',
-    avatar: '/img/creators/c1.png',
-    totalSales: '34.53',
+    name: "Keepitreal",
+    rank: "1",
+    avatar: "/img/creators/c1.png",
+    totalSales: "34.53",
   },
   {
-    name: 'DigiLab',
-    rank: '2',
-    avatar: '/img/creators/c2.png',
-    totalSales: '34.53',
+    name: "DigiLab",
+    rank: "2",
+    avatar: "/img/creators/c2.png",
+    totalSales: "34.53",
   },
   {
-    name: 'GravityOne',
-    rank: '3',
-    avatar: '/img/creators/c3.png',
-    totalSales: '34.53',
+    name: "GravityOne",
+    rank: "3",
+    avatar: "/img/creators/c3.png",
+    totalSales: "34.53",
   },
   {
-    name: 'Juanie',
-    rank: '4',
-    avatar: '/img/creators/c4.png',
-    totalSales: '34.53',
+    name: "Juanie",
+    rank: "4",
+    avatar: "/img/creators/c4.png",
+    totalSales: "34.53",
   },
   {
-    name: 'Juanie',
-    rank: '4',
-    avatar: '/img/creators/c4.png',
-    totalSales: '34.53',
+    name: "Juanie",
+    rank: "4",
+    avatar: "/img/creators/c4.png",
+    totalSales: "34.53",
   },
   {
-    name: 'Juanie',
-    rank: '4',
-    avatar: '/img/creators/c4.png',
-    totalSales: '34.53',
+    name: "Juanie",
+    rank: "4",
+    avatar: "/img/creators/c4.png",
+    totalSales: "34.53",
   },
   {
-    name: 'Juanie',
-    rank: '4',
-    avatar: '/img/creators/c4.png',
-    totalSales: '34.53',
+    name: "Juanie",
+    rank: "4",
+    avatar: "/img/creators/c4.png",
+    totalSales: "34.53",
   },
   {
-    name: 'Juanie',
-    rank: '4',
-    avatar: '/img/creators/c4.png',
-    totalSales: '34.53',
+    name: "Juanie",
+    rank: "4",
+    avatar: "/img/creators/c4.png",
+    totalSales: "34.53",
   },
 ];
 function TopCreators() {
@@ -57,11 +57,16 @@ function TopCreators() {
       <h2>Top Creators</h2>
       <div className="btn-container">
         <h3>Checkout Top Rated Creators on the NFT Marketplace</h3>
-        <Button btnName='View Rankings' btnType='secondary-outlined' icon={RocketLaunch}/>
+        <Button
+          btnName="View Rankings"
+          btnType="secondary-outlined"
+          icon={RocketLaunch}
+        />
       </div>
       <div className="creator-rank-list">
         {creators.map((creator, index) => (
           <div key={index} className="creator-card">
+            <div className="creator-rank">{creator.rank}</div>
             <img src={creator.avatar} alt="avatar" />
             <h4>{creator.name}</h4>
             <div>
