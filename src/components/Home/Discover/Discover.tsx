67ce9ed8e79a634b553b088d2454b014b7cc4b90
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-02-13 15:03:36 +0530

    Made the ui responsive for smaller screens

--- src/components/Home/Discover/Discover.tsx
+++ src/components/Home/Discover/Discover.tsx
@@ -1,31 +1,31 @@
-import Button from '../../common/Button/Button';
-import SeeIcon from '../../../assests/icons/Eye.svg'
-import './Discover.scss';
+import Button from "../../common/Button/Button";
+import SeeIcon from "../../../assests/icons/Eye.svg";
+import "./Discover.scss";
 
 const nftData = [
   {
-    titile: 'Distant Galaxy',
-    data: '/img/discover/nft1.png',
-    artist: 'Moondancer',
-    artistAvatar: '/img/discover/a1.png',
-    price: '1.63',
-    highestBid: '0.33',
+    titile: "Distant Galaxy",
+    data: "/img/discover/nft1.png",
+    artist: "Moondancer",
+    artistAvatar: "/img/discover/a1.png",
+    price: "1.63",
+    highestBid: "0.33",
   },
   {
-    titile: 'Life on Edena',
-    data: '/img/discover/nft2.png',
-    artist: 'NebulaKid',
-    artistAvatar: '/img/discover/a2.png',
-    price: '1.63',
-    highestBid: '0.33',
+    titile: "Life on Edena",
+    data: "/img/discover/nft2.png",
+    artist: "NebulaKid",
+    artistAvatar: "/img/discover/a2.png",
+    price: "1.63",
+    highestBid: "0.33",
   },
   {
-    titile: 'Astro Fiction',
-    data: '/img/discover/nft3.png',
-    artist: 'Spaceone',
-    artistAvatar: '/img/discover/a3.png',
-    price: '1.63',
-    highestBid: '0.33',
+    titile: "Astro Fiction",
+    data: "/img/discover/nft3.png",
+    artist: "Spaceone",
+    artistAvatar: "/img/discover/a3.png",
+    price: "1.63",
+    highestBid: "0.33",
   },
 ];
 function Discover() {
@@ -34,10 +34,10 @@ function Discover() {
       <h2>Discover More NFTs</h2>
       <div className="btn-container">
         <h3>Explore New Trending NFTs</h3>
-        <Button btnName='See All' btnType='secondary-outlined' icon={SeeIcon}/>
+        <Button btnName="See All" btnType="secondary-outlined" icon={SeeIcon} />
       </div>
       <div className="nftCardList">
-        {nftData.map((nft, index) => (
+        {nftData.slice(0, 3).map((nft, index) => (
           <div key={index} className="nftCard">
             <img src={nft.data} />
             <h4>{nft.titile}</h4>
--- src/components/Home/GetStarted/GetStarted.tsx
+++ src/components/Home/GetStarted/GetStarted.tsx
@@ -1,26 +1,26 @@
-import './GetStarted.scss';
+import "./GetStarted.scss";
 
 const data = [
   {
-    icon: '/img/info/wallet-icon.png',
-    alticon: 'Wallet Icon',
-    title: 'Setup Your Wallet',
+    icon: "/img/info/wallet-icon.png",
+    alticon: "Wallet Icon",
+    title: "Setup Your Wallet",
     details:
-      'Set up your wallet of choice. Connect it to the Animarket by clicking the wallet icon in the top right corner',
+      "Set up your wallet of choice. Connect it to the Animarket by clicking the wallet icon in the top right corner",
   },
   {
-    icon: '/img/info/collection-icon.png',
-    alticon: 'Collection Icon',
-    title: 'Create Collection',
+    icon: "/img/info/collection-icon.png",
+    alticon: "Collection Icon",
+    title: "Create Collection",
     details:
-      'Upload your work and setup your collection. Add a description, social links and floor price.',
+      "Upload your work and setup your collection. Add a description, social links and floor price.",
   },
   {
-    icon: '/img/info/earn-icon.png',
-    alticon: 'Earning Icon',
-    title: 'Start Earning',
+    icon: "/img/info/earn-icon.png",
+    alticon: "Earning Icon",
+    title: "Start Earning",
     details:
-      'Choose between auctions and fixed-price listings. Start earning by selling your NFTs or trading others.',
+      "Choose between auctions and fixed-price listings. Start earning by selling your NFTs or trading others.",
   },
 ];
 function GetStarted() {
@@ -32,8 +32,10 @@ function GetStarted() {
         {data.map((info, index) => (
           <div key={index} className="info-card">
             <img src={info.icon} alt={info.alticon} className="icon" />
-            <h4>{info.title}</h4>
-            <span>{info.details}</span>
+            <div className="text-info">
+              <h4>{info.title}</h4>
+              <span>{info.details}</span>
+            </div>
           </div>
         ))}
       </div>
