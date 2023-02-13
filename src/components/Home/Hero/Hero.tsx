86fd5301bca40094505349aa9861f041d13a433b
sanketdebnath24 <sanket.debnath@redsoftware.in>
2023-02-13 18:11:44 +0530

    Ui update for smaller screens

--- src/components/Home/Hero/Hero.tsx
+++ src/components/Home/Hero/Hero.tsx
@@ -1,22 +1,26 @@
-import Button from '../../common/Button/Button';
-import RocketLaunch from "../../../assests/icons/RocketLaunch.svg"
-import './Hero.scss';
+import Button from "../../common/Button/Button";
+import RocketLaunch from "../../../assests/icons/RocketLaunch.svg";
+import "./Hero.scss";
 
-const totalSales = '240k';
-const totalArtists = '240k';
-const totalAuctions = '100k';
+const totalSales = "240k";
+const totalArtists = "240k";
+const totalAuctions = "100k";
 
 const nft = {
-  name: 'Space Walking',
-  artist: 'Animakid',
-  data: '/img/hero/nft.png',
+  name: "Space Walking",
+  artist: "Animakid",
+  data: "/img/hero/nft.png",
+  artistAvatar: "/img/hero/a.png",
 };
 const HighlightedNFT = () => {
   return (
     <div className="nft-card">
       <img src={nft.data} />
       <h2>{nft.name}</h2>
-      <h3>{nft.artist}</h3>
+      <div>
+        <img src={nft.artistAvatar} />
+        <h3>{nft.artist}</h3>
+      </div>
     </div>
   );
 };
@@ -27,17 +31,23 @@ function Hero() {
       <div className="hero-intro">
         <h1>Discover digital art & Collect NFTs</h1>
         <p>
-          NFT marketplace UI created with Anima for Figma. Collect, buy and
-          sell art from more than 20k NFT artists.
+          NFT marketplace UI created with Anima for Figma. Collect, buy and sell
+          art from more than 20k NFT artists.
         </p>
-        <Button btnName='Get Started' btnType='secondary-filled' icon={RocketLaunch}/>
-        <div className="site-info">
-          <span>{totalSales}+</span>
-          <span>{totalAuctions}+</span>
-          <span>{totalArtists}+</span>
-          <div>Total Sales</div>
-          <div>Auctions</div>
-          <div>Artists</div>
+        <div className="hero-btn-container">
+          <Button
+            btnName="Get Started"
+            btnType="secondary-filled"
+            icon={RocketLaunch}
+          />
+          <div className="site-info">
+            <span>{totalSales}+</span>
+            <span>{totalAuctions}+</span>
+            <span>{totalArtists}+</span>
+            <div>Total Sales</div>
+            <div>Auctions</div>
+            <div>Artists</div>
+          </div>
         </div>
       </div>
       <HighlightedNFT />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
-import BrowseCategories from '../components/Home/BroweCategories/BrowseCategories';
-import Digest from '../components/Home/Digest/Digest';
-import Discover from '../components/Home/Discover/Discover';
-import GetStarted from '../components/Home/GetStarted/GetStarted';
-import Hero from '../components/Home/Hero/Hero';
-import TopCreators from '../components/Home/TopCreators/TopCreators';
-import TrendingCollection from '../components/Home/TrendingCollection/TrendingCollection';
+import BrowseCategories from "../components/Home/BroweCategories/BrowseCategories";
+import Digest from "../components/Home/Digest/Digest";
+import Discover from "../components/Home/Discover/Discover";
+import GetStarted from "../components/Home/GetStarted/GetStarted";
+import Hero from "../components/Home/Hero/Hero";
+import TopCreators from "../components/Home/TopCreators/TopCreators";
+import TrendingCollection from "../components/Home/TrendingCollection/TrendingCollection";
 
 function Home() {
   return (
